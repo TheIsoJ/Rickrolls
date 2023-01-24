d131afbba14840b85e7b159373a5b65393c5413a
Jesse Keskelä <keskelajesse@gmail.com>
2023-01-24 11:35:42 +0000

    Valmis

--- server/prisma/seed.js
+++ server/prisma/seed.js
@@ -4,11 +4,14 @@ const prisma = new PrismaClient()
 import dotenv from "dotenv"
 dotenv.config()
 
+import slugify from "slugify"
+
 async function main() {
   await prisma.rickroll.deleteMany()
   await prisma.user.deleteMany()
 
   const slug1 = "Eka rickroll"
+  const slug2 = "En keksi mitään, saatana! Android, kerro sä!"
 
   await prisma.user.create({
     data: {
@@ -32,9 +35,11 @@ async function main() {
         id: "f56d64a6-f228-4a52-8347-87286de56160",
         name: "Eka rickroll",
         description: "Eka rickroll",
-        slug: slug1
-          .replace(" ", "-")
-          .toLowerCase(),
+        slug: slugify.default(slug1, {
+          locale: "fi",
+          lower: true,
+          trim: true
+        }),
         link: "https://www.youtube.com/watch?v=dQw4w9WgXcQ",
         videoId: "dQw4w9WgXcQ",
         rickroll_cta_link:
@@ -44,7 +49,11 @@ async function main() {
         id: "50996030-f78b-48db-9561-df533f350fa6",
         name: "En keksi mitään, saatana! Android, kerro sä!",
         description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.",
-        slug: "en-keksi-mitaan--saatana--android--kerro-sa",
+        slug: slugify.default(slug2, {
+          locale: "fi",
+          lower: true,
+          trim: true
+        }),
         link: "https://www.youtube.com/watch?v=dQw4w9WgXcQ",
         videoId: "dQw4w9WgXcQ",
         rickroll_cta_link: "https://images.jesunmaailma.ml/rickrolls-api-images/risitas.jpg"
