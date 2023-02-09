3abb0f21c83f89b27954788a324b2c4bdf002314
Jesse Keskelä <monkeydie_@hotmail.com>
2023-02-09 11:59:12 +0200

    update client/pages/_app.tsx

--- client/pages/_app.tsx
+++ client/pages/_app.tsx
@@ -1,34 +1,11 @@
 import "../styles/globals.css"
 import type { AppProps } from "next/app"
-import "../public/css/nprogress.css"
-import NProgress from "nprogress"
 import { UserProvider } from "@auth0/nextjs-auth0/client"
 import Footer from "../components/Footer"
-import { useRouter } from "next/router"
 import { useEffect } from "react"
 import "../public/css/rickroll.styles.css"
 
 function MyApp({ Component, pageProps }: AppProps) {
-  const router = useRouter()
-
-  useEffect(() => {
-    const handleStart = () => {
-      NProgress.start()
-    }
-    const handleStop = () => {
-      NProgress.done()
-    }
-
-    router.events.on("routeChangeStart", handleStart)
-    router.events.on("routeChangeComplete", handleStop)
-    router.events.on("routeChangeError", handleStop)
-
-    return () => {
-      router.events.off("routeChangeStart", handleStart)
-      router.events.off("routeChangeComplete", handleStop)
-      router.events.off("routeChangeError", handleStop)
-    }
-  }, [router])
 
   return (
     <>
